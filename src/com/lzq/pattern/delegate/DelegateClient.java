5ca20e570464b1f057dc58506e5525fb5b4305ce
LUO <13193865768@163.com>
2022-09-03 18:50:36 +0800

    前进

--- src/com/lzq/pattern/delegate/DelegateClient.java
+++ src/com/lzq/pattern/delegate/DelegateClient.java
@@ -3,5 +3,7 @@ package com.lzq.pattern.delegate;
 public class DelegateClient {
     public static void main(String[] args) {
         System.out.println("111");
+        System.out.println("2222");
+        System.out.println(3333);
     }
 }
