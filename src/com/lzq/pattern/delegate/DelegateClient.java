013f61c9372316f62b7e76780408a2c344e178eb
LUO <13193865768@163.com>
2022-09-03 18:35:00 +0800

    测试方法2

--- src/com/lzq/pattern/delegate/DelegateClient.java
+++ src/com/lzq/pattern/delegate/DelegateClient.java
@@ -3,6 +3,5 @@ package com.lzq.pattern.delegate;
 public class DelegateClient {
     public static void main(String[] args) {
         System.out.println("111");
-        System.out.println("2222");
     }
 }
